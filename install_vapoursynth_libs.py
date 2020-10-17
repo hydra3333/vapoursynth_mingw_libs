c92aaf37df42c139d01c1602a55c0bd4b4f57497
hydra3333 <hydra3333@gmail.com>
2020-10-17 18:05:37 +1030

    test if it works with python 3.9.0

--- install_vapoursynth_libs.py
+++ install_vapoursynth_libs.py
@@ -102,8 +102,13 @@ else:
 		runCmd("cp {0} ../bin".format("VSScript.dll"))
 		runCmd("cp {0} ../bin".format("VapourSynth.dll"))
 		
-		pydName = "vapoursynth.cp36-win_amd64.pyd"
-		if float(ver_suff) >= 49: # Version 49 and above use python 3.8
+		pydName = "vapoursynth.cp38-win_amd64.pyd"
+		if float(ver_suff) >= 52: # Version 52 and above use python 3.8 and perhaps even works with 3.9
+			#pydName = "vapoursynth.cp38-win_amd64.pyd"
+			#VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.8").replace("%%PY_VER%%","38")
+			pydName = "vapoursynth.cp39-win_amd64.pyd"
+			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.9").replace("%%PY_VER%%","39")
+		elif float(ver_suff) >= 49: # Version 49 and above use python 3.8 and perhaps even works with 3.9
 			pydName = "vapoursynth.cp38-win_amd64.pyd"
 			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.8").replace("%%PY_VER%%","38")
 		elif float(ver_suff) > 44: # Version 45 and above use python 3.7		
