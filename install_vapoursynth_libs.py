c721f381ef0c33043363a08238056564701c7495
hydra3333 <hydra3333@gmail.com>
2020-11-01 12:47:26 +1030

    R53 can use python3.9, not 52

--- install_vapoursynth_libs.py
+++ install_vapoursynth_libs.py
@@ -103,7 +103,7 @@ else:
 		runCmd("cp {0} ../bin".format("VapourSynth.dll"))
 		
 		pydName = "vapoursynth.cp38-win_amd64.pyd"
-		if float(ver_suff) >= 53: # Version 52 and above perhaps even works with 3.9
+		if float(ver_suff) >= 53: # Version 53 and above perhaps even works with 3.9
 			pydName = "vapoursynth.cp39-win_amd64.pyd"
 			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.9").replace("%%PY_VER%%","39")
 		elif float(ver_suff) >= 49: # Version 49 through 52 use python 3.8 only
